5a1c52dfdb23e98d01c50dae927e39b310468fe5
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-02-23 14:40:24 +0100

    Création de la base de donnée

--- app/src/main/java/fr/iutamiens/lakraao/note/MainActivity.java
+++ app/src/main/java/fr/iutamiens/lakraao/note/MainActivity.java
@@ -1,13 +1,30 @@
 package fr.iutamiens.lakraao.note;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DatabaseOpenHelper openHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openHelper = new DatabaseOpenHelper(this);
+        SQLiteDatabase database = openHelper.getReadableDatabase();
+        Cursor cursor = database.rawQuery("SELECT * FROM todo", null);
+
+        int idIndex = cursor.getColumnIndex("id");
+        int nameIndex = cursor.getColumnIndex("name");
+        while(cursor.moveToNext()){
+            int id = cursor.getInt(idIndex);
+            int name = cursor.getInt(nameIndex);
+            Log.d("Query", "id = " + id + "| name = " + name);
+        }
+        cursor.close();
+
     }
 }
