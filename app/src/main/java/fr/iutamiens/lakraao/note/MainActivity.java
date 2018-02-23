497e52ed3558ae0534a60984a9ff9a626a78290d
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-02-23 15:12:05 +0100

    Ajout de l'EditText dans le dialog

--- app/src/main/java/fr/iutamiens/lakraao/note/MainActivity.java
+++ app/src/main/java/fr/iutamiens/lakraao/note/MainActivity.java
@@ -11,9 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -37,21 +34,24 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 
-                //LayoutInflater layoutInflater = getLayoutInflater();
-                //View dialogView = layoutInflater.inflate(R.layout.dialog_test, null);
-                builder.setTitle("test");
+                LayoutInflater layoutInflater = getLayoutInflater();
+                View dialogView = layoutInflater.inflate(R.layout.dialog_view, null);
+                builder.setView(dialogView);
+                builder.setTitle("Créer une nouvelle Note");
 
                 /**
                  * Bouton Valider
                  */
                 builder.setPositiveButton("Valider", new DialogInterface.OnClickListener(){
+                    private EditText input;//Input de la fenêtre de dialog
 
                     @Override
                     public void onClick(DialogInterface dialogInterface, int which) {
                         AlertDialog alertDialog = (AlertDialog) dialogInterface;
-                        addItem("test");
+                        input = alertDialog.findViewById(R.id.dialog_input);
+                        addItem(input.getText().toString());
                         Log.d("Dialog", "Valider");
                     }
                 });
