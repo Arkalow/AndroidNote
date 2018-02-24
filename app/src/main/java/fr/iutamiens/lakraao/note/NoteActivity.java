ff812c638eb88c08473ccd08f61cbe946f0f8775
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-02-24 22:07:39 +0100

    Ajout du menu dans NotActivity

--- app/src/main/java/fr/iutamiens/lakraao/note/NoteActivity.java
+++ app/src/main/java/fr/iutamiens/lakraao/note/NoteActivity.java
@@ -3,6 +3,9 @@ package fr.iutamiens.lakraao.note;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class NoteActivity extends AppCompatActivity {
@@ -25,5 +28,35 @@ public class NoteActivity extends AppCompatActivity {
         Note note = NoteManage.select(noteId, DatabaseOpenHelper.getSelf(this));
         title.setText(note.getTitle());
         content.setText(note.getContent());
+    }
+    /***
+     * Créer le menu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.note_menu, menu);
+        return true;
+    }
+    /***
+     * Selection d'un bouton du menu
+     * @param item Selected item
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.share){
+
+        }
+        switch (item.getItemId()){
+            case R.id.share:
+                Log.d("Menu", "share");
+                break;
+            case R.id.edit:
+                Log.d("Menu", "edit");
+                break;
+            case R.id.delete:
+                Log.d("Menu", "delete");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
